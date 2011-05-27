83860754311e0bd1cffd56d790ab85c0bd03c8df
Phillip Schichtel <quick_wango@code-infection.de>
2011-05-27 14:42:40 +0200

    added scroller to the overlay

--- backend/templates/generic/worldutils.tpl.php
+++ backend/templates/generic/worldutils.tpl.php
@@ -1,20 +1,28 @@
 <?php $lang = Lang::instance('worldutils') ?>
 <div id="world_overlay">
-    <ul class="rounded">
-        <li class="arrow"><a href="#" id="world_info"><?php $lang->infos ?></a></li>
-        <li class="arrow"><a href="#" id="world_playerlist"><?php $lang->playerlist ?></a></li>
-        <li><a href="#" id="world_time"><?php $lang->time ?></a></li>
-        <li><a href="#" id="world_storm"><?php $lang->storm ?></a></li>
-        <li><a href="#" id="world_pvp"><?php $lang->pvp ?></a></li>
-        <li><a href="#" id="world_spawn"><?php $lang->spawn ?></a></li>
-        <!--
-        <li><a href="#" id="world_weather"><?php $lang->weather ?></a></li>
-        <li><a href="#" id="world_thunder"><?php $lang->thunder ?></a></li>
-        -->
-    </ul>
-    <ul class="rounded">
-        <li><a href="#" class="toggleoverlay"><?php $lang->close ?></a></li>
-    </ul>
+    <div id="world_scroller">
+        <div>
+            <ul class="rounded">
+                <li class="arrow"><a href="#" id="world_info"><?php $lang->infos ?></a></li>
+                <li class="arrow"><a href="#" id="world_playerlist"><?php $lang->playerlist ?></a></li>
+                <li><a href="#" id="world_time"><?php $lang->time ?></a></li>
+                <li><a href="#" id="world_storm"><?php $lang->storm ?></a></li>
+                <li><a href="#" id="world_pvp"><?php $lang->pvp ?></a></li>
+                <li><a href="#" id="world_spawn"><?php $lang->spawn ?></a></li>
+                <!--
+                <li><a href="#" id="world_weather"><?php $lang->weather ?></a></li>
+                <li><a href="#" id="world_thunder"><?php $lang->thunder ?></a></li>
+                -->
+            </ul>
+            <ul class="rounded">
+                <li><a href="#" class="toggleoverlay"><?php $lang->close ?></a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <script type="text/javascript" src="backend/javascriptlang.php?file=worldutils"></script>
 <script type="text/javascript" src="js/worldutils.js"></script>
+<script type="text/javascript" src="js/iscroll-lite.min.js"></script>
+<script type="text/javascript">
+    var scroller = new iScroll('world_overlay');
+</script>
