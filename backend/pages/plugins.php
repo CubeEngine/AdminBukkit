bbc65d463479c42d8575bebd17ba38d62dc14fab
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-11 19:56:58 +0200

    fixed plugins page

--- backend/pages/plugins.php
+++ backend/pages/plugins.php
@@ -1,11 +1,10 @@
 <?php
     $lang = Lang::instance('plugins');
     $page = new Page('plugins', $lang['pluginlist'], true);
-    $toolbar->setBack(Lang::instance('generic')->get('btn_back'));
-    $toolbar->setButton($lang['refresh'], '#');
-    $page->addSubtemplate('toolbar', $toolbar);
-    $page->setContent(new Template('pages/plugins'));
-    $page->setInfo($lang['info']);
+    $page->setBack(Lang::instance('generic')->get('btn_back'))
+         ->setButton($lang['refresh'], '#')
+         ->setContent(new Template('pages/plugins'))
+         ->setInfo($lang['info']);
     
     $design->setContentView($page);
 ?>
