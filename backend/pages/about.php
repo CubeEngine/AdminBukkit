bd3cc6db55871cbdc4f5d33aad7ea6cc66f67ea2
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-11 20:35:03 +0200

    fixed notice in about page

--- backend/pages/about.php
+++ backend/pages/about.php
@@ -1,9 +1,8 @@
 <?php
     $lang = Lang::instance('about');
     $page = new Page('about', $lang['about']);
-    $page->setBack(Lang::instance('generic')->get('btn_back'));
-    $page->addSubtemplate('toolbar', $toolbar);
-    $page->setContent(new Template('pages/about'));
+    $page->setBack(Lang::instance('generic')->get('btn_back'))
+         ->setContent(new Template('pages/about'));
     
     $design->setContentView($page);
 ?>
