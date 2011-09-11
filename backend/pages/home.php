14110020031369668ddade6f70b67862f9c8a5e4
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-11 14:18:16 +0200

    fixed home page

--- backend/pages/home.php
+++ backend/pages/home.php
@@ -1,9 +1,8 @@
 <?php
     $lang = Lang::instance('home');
     $page = new Page('home', $lang['home']);
-    $page->setButton($lang['about'], $design->getLinkGenerator()->page('about'));
-    $page->addSubtemplate('toolbar', $toolbar);
-    $page->setContent(new Template('pages/home'));
+    $page->setButton($lang['about'], $design->getLinkGenerator()->page('about'))
+         ->setContent(new Template('pages/home'));
     
     $design->setContentView($page);
 ?>
