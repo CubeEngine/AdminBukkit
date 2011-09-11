04dafb322a87fe3ae55e7ffc6ff5da425e7f8ddc
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-11 20:23:02 +0200

    fixed profile page

--- backend/templates/pages/profile.tpl.php
+++ backend/templates/pages/profile.tpl.php
@@ -1,8 +1,8 @@
 <?php $lang = Lang::instance('profile') ?>
 <h2><?php $lang->userinfos ?>:</h2>
-<ul class="rounded">
-    <li><?php $lang->username ?>: <?php echo $username ?></li>
-    <li><?php $lang->email ?>: <?php echo $email ?></li>
-    <li><?php $lang->apiaddr ?>: <?php echo $host . ':' . $port ?></li>
-    <li><?php $lang->apipass ?>: <?php echo htmlspecialchars($pass) ?></li>
+<ul data-role="listview" data-inset="true">
+    <li><?php $lang->username ?>: <span class="value"><?php echo $username ?></span></li>
+    <li><?php $lang->email ?>: <span class="value"><?php echo $email ?></span></li>
+    <li><?php $lang->apiaddr ?>: <span class="value"><?php echo $host . ':' . $port ?></span></li>
+    <li><?php $lang->apipass ?>: <span class="value"><?php echo htmlspecialchars($pass) ?></span></li>
 </ul>
