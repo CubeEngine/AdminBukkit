398003b6df50a843bd4057047a68232bf6db66c4
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-19 22:29:28 +0200

    re-enable player name validation on editprofile page

--- backend/pages/editprofile.php
+++ backend/pages/editprofile.php
@@ -26,7 +26,7 @@
         }
         elseif (!preg_match('/^[\w\d-]{5,40}/i', $user))
         {
-            //$errors[] = $registerLang['user_invalid']; // @todo reenabled error
+            $errors[] = $registerLang['user_invalid'];
         }
         if (empty($email))
         {
