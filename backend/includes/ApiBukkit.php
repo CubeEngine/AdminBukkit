22c09794f91c4e07367ac8914662112eb9660d40
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-20 23:37:16 +0200

    added a doc block to ApiBukkit::requestPath

--- backend/includes/ApiBukkit.php
+++ backend/includes/ApiBukkit.php
@@ -14,7 +14,14 @@
             $this->http = $http;
             $this->pass = $pass;
         }
-        
+
+        /**
+         * Requests a path on the API server with the given params and returns the HTTP response
+         *
+         * @param string $path the path to request on the API server
+         * @param array $params the params to pass to the API server
+         * @return HttpReply the response from the API server
+         */
         public function requestPath($path, array $params = array())
         {
             $path = '/' . ltrim(trim($path), '/');
