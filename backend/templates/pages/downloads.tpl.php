02735bf989a90b46c0041dc78200cf08cfe6b9c7
Phillip Schichtel <quick_wango@code-infection.de>
2011-09-20 23:16:40 +0200

    worked around the problems on downloads page and fixed the theming

--- backend/templates/pages/downloads.tpl.php
+++ backend/templates/pages/downloads.tpl.php
@@ -1,11 +1,12 @@
 <?php $lang = Lang::instance('downloads') ?>
 <ul data-role="listview">
-    <li data-role="divider"><?php $lang->baseplugin ?></li>
-    <li><a class="download" href="<?php $this->page('downloads') ?>?file=ApiBukkit.jar">ApiBukkit</a></li>
+    <li data-role="divider" data-theme="e"><?php $lang->baseplugin ?></li>
+    <li><a target="_blank" class="download" href="<?php $this->page('downloads') ?>?file=ApiBukkit.jar">ApiBukkit</a></li>
 
-    <li data-role="divider"><?php $lang->apiplugins ?></li>
-    <li class="arrow"><a class="download" href="<?php $this->page('downloads') ?>?file=BasicApi.jar">BasicApi</a></li>
+    <li data-role="divider" data-theme="e"><?php $lang->apiplugins ?></li>
+    <li><a target="_blank" class="download" href="<?php $this->page('downloads') ?>?file=BasicApi.jar">BasicApi</a></li>
 </ul>
+<!--
 <script type="text/javascript">
     $('#downloads a.download').click(function(e){
         //document.location.href = $(e.target).attr('href');
@@ -18,3 +19,4 @@
         e.preventDefault();
     });
 </script>
+-->
