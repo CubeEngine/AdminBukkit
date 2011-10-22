cfff776906b8077e65072f1e2122067c8849fa94
Phillip Schichtel <quick_wango@code-infection.de>
2011-10-22 03:21:18 +0200

    added a better page title

--- backend/templates/generic/page.tpl.php
+++ backend/templates/generic/page.tpl.php
@@ -1,5 +1,5 @@
 <?php $lang = Lang::instance('generic') ?>
-<div id="<?php echo $pageName ?>" data-role="page" data-theme="a">
+<div id="<?php echo $pageName ?>" data-role="page" data-title="AdminBukkit - <?php echo $pageTitle ?>" data-theme="a">
     <div data-role="header">
         <?php if (isset($backText)): ?>
             <?php if (isset($backTarget)): ?>
