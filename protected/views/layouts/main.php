daf4fbbf9f3bd3a6c2bd4fab59549d3e0bacd073
Phillip Schichtel <quick_wango@code-infection.de>
2012-01-13 22:25:14 +0100

    fixed the viewport syntax in the main layout

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php echo CHtml::encode(Yii::app()->name); ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl ?>/res/jquery/jquery.mobile.css">
